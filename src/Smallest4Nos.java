cc966c31253d10d0223fe5732e9159c826e5b6e1
Ashwini <ashanika25@gmail.com>
2021-11-12 08:19:06 +0530

    Smallest of 4 Numbers

---
+++ src/Smallest4Nos.java
@@ -0,0 +1,39 @@
+/* Program to find smallest of 4 numbers */
+
+import java.util.Scanner;
+
+public class Smallest4Nos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 4 numbers to find smallest");
+        System.out.println("Enter first number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter seconnd number");
+        int num2 =sc.nextInt();
+        System.out.println("Enter third number");
+        int num3 = sc.nextInt();
+        System.out.println("Enter fourth number");
+        int num4 = sc.nextInt();
+
+        //Using ternary operator
+        int smallest = (num1<num2 && num2<num3 && num3<num4)? num1:((num2<num3&&num2<num4)?num2:(num3<num4?num3:num4));
+
+        /* Using if else if condition
+        int smallest;
+        if(num1<num2 && num2<num3 && num3<num4){
+             smallest = num1;
+        }
+        else if(um2<num3&&num2<num4){
+             smallest = num2;
+         }
+         else if(num3<num4){
+             smallest = num3;
+         }else {
+             smallest = num4;
+         }
+         */
+
+        System.out.println("The smallest of 4 numbers is " + smallest);
+
+    }
+}
